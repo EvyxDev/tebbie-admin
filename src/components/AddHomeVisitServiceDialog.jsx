6faa7f8360ce289b323264ba9f5e9ca2b2edc29d
Abdulrahmanfawzy <abdulrahman2d77@gmail.com>
2025-12-08 16:08:09 +0200

    remove price field from home service form

--- src/components/AddHomeVisitServiceDialog.jsx
+++ src/components/AddHomeVisitServiceDialog.jsx
@@ -186,22 +186,6 @@ const AddHomeVisitServiceDialog = ({ isOpen, onClose, editData }) => {
           </div>
 
           {/* Price */}
-          <div>
-            <label className="block text-sm font-medium text-gray-700 mb-1">
-              {t("price")}
-            </label>
-            <input
-              type="number"
-              name="price"
-              value={formData.price}
-              onChange={handleInputChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder={t("price")}
-              min="0"
-              step="0.01"
-              required
-            />
-          </div>
 
           {/* Hospital Price */}
           <div>
